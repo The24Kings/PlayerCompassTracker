9d952652d81c8dea1853fe540271403558c2f2b4
24Kings <24kingsunite@gmail.com>
2021-05-25 10:46:53 -0700

    Tried doing a thing, did not work

--- src/main/java/compass/tracker/PlayerEventHandler.java
+++ src/main/java/compass/tracker/PlayerEventHandler.java
@@ -10,12 +10,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
 
-//TODO Change this to check every 20 ticks for when the prey is moving while the hunter is holding a compass to update the CompassMeta
+//TODO Change this to check every tick for when the prey is moving while the hunter is holding a compass to update the CompassMeta
 public class PlayerEventHandler implements Listener {
+    /*
     @EventHandler
     public void onUpdateCompass(PlayerInteractEvent event) {
         if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
@@ -36,6 +40,30 @@ public class PlayerEventHandler implements Listener {
                     } else player.sendMessage(ChatColor.DARK_RED + "Could not find player!");
                 } else player.sendMessage(ChatColor.RED + "Error finding your prey, please try /hunt again!");
             }
+        }
+    }
+*/
+    @EventHandler
+    public void upDateCompass(PlayerMoveEvent event) {
+        if (event.getPlayer().getActiveItem().equals(new ItemStack(Material.AIR)) || !event.getPlayer().getActiveItem().getItemMeta().hasDisplayName()) return;
+
+        //First IF statement throws null pointer exception
+        if (event.getPlayer().getActiveItem().equals(new ItemStack(Material.COMPASS)) && PlainComponentSerializer.plain().serialize(event.getPlayer().getActiveItem().getItemMeta().displayName()).contains("Tracker")) {
+            CompassTracker.getPlugin().getLogger().warning("made is past first if statement"); //log
+            CompassMeta compassMeta = (CompassMeta) event.getPlayer().getActiveItem().getItemMeta();
+            PersistentDataContainer data = compassMeta.getPersistentDataContainer();
+            Player player = event.getPlayer();
+
+            if (data.has(new NamespacedKey(CompassTracker.getPlugin(), "player"), PersistentDataType.STRING)) {
+                CompassTracker.getPlugin().getLogger().warning("made is past second if statement"); //log
+                Player prey = Bukkit.getPlayerExact(data.get(new NamespacedKey(CompassTracker.getPlugin(), "player"), PersistentDataType.STRING));
+
+                if (prey != null && prey.isOnline() && (event.getPlayer().equals(prey) && event.getTo() != event.getFrom())) {
+                    CompassTracker.getPlugin().getLogger().warning("made is past third if statement"); //log
+                    compassMeta.setLodestone(prey.getLocation());
+                    event.getPlayer().getActiveItem().setItemMeta(compassMeta);
+                } else player.sendMessage(ChatColor.DARK_RED + "Could not find player!");
+            } else player.sendMessage(ChatColor.RED + "Error finding your prey, please try /hunt again!");
         }
     }
 }
