929f9f43737b5dad15c8a462265176d4c12f404c
24Kings <24kingsunite@gmail.com>
2021-05-26 10:40:04 -0700

    Increased delay and re-enabled right click functionality

--- src/main/java/compass/tracker/PlayerEventHandler.java
+++ src/main/java/compass/tracker/PlayerEventHandler.java
@@ -1,6 +1,5 @@
 package compass.tracker;
 
-import net.kyori.adventure.audience.MessageType;
 import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -17,11 +16,8 @@ import org.bukkit.inventory.meta.CompassMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.jetbrains.annotations.NotNull;
 
-//TODO Change this to check every tick for when the prey is moving while the hunter is holding a compass to update the CompassMeta
 public class PlayerEventHandler implements Listener {
-    /*
     @EventHandler
     public void onUpdateCompass(PlayerInteractEvent event) {
         if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
@@ -36,19 +32,21 @@ public class PlayerEventHandler implements Listener {
                     Player prey = Bukkit.getPlayerExact(data.get(new NamespacedKey(CompassTracker.getPlugin(), "player"), PersistentDataType.STRING));
 
                     if (prey != null && prey.isOnline()) {
-                        compassMeta.setLodestone(prey.getLocation());
-                        event.getItem().setItemMeta(compassMeta);
-                        player.sendMessage(ChatColor.DARK_GREEN + "Updated location of " + ChatColor.RESET + PlainComponentSerializer.plain().serialize(prey.displayName()));
-                    } else player.sendMessage(ChatColor.DARK_RED + "Could not find player!");
-                } else player.sendMessage(ChatColor.RED + "Error finding your prey, please try /hunt again!");
+                        if (!event.getPlayer().equals(prey)) {
+                            compassMeta.setLodestone(prey.getLocation());
+                            event.getItem().setItemMeta(compassMeta);
+                            player.sendMessage(ChatColor.DARK_GREEN + "Updated location of " + ChatColor.RESET + PlainComponentSerializer.plain().serialize(prey.displayName()));
+                        } else player.sendMessage(ChatColor.RED + "Cannot track yourself!");
+                    } else CompassTracker.getPlugin().getLogger().severe(ChatColor.DARK_RED + "Could not find player!");
+                } else CompassTracker.getPlugin().getLogger().severe(ChatColor.RED + "Error finding your prey, please try /hunt again!");
             }
         }
     }
-*/
+
     @EventHandler
-    public void upDateCompass(PlayerMoveEvent event) {
+    public void updateCompass(PlayerMoveEvent event) {
         ItemStack held = event.getPlayer().getInventory().getItemInMainHand();
-        int delay = 20; //# of ticks
+        int delay = 20 * 5; /* Seconds delay */
 
         if (held.getType().equals(Material.AIR) || !held.getItemMeta().hasDisplayName()) return;
 
@@ -66,16 +64,16 @@ public class PlayerEventHandler implements Listener {
                             @Override
                             public void run() {
                                 Compass.toggle(); //Toggle runnable on
-                                if (/* !event.getPlayer().equals(prey) && */ event.getTo() != event.getFrom()) {
+                                if (!event.getPlayer().equals(prey) && event.getTo() != event.getFrom()) {
                                     compassMeta.setLodestone(prey.getLocation());
                                     held.setItemMeta(compassMeta);
-                                    CompassTracker.getPlugin().getLogger().info(ChatColor.GREEN + "Now pointing to: " + ChatColor.GOLD + prey.getName() + ChatColor.RESET + " at [" + prey.getLocation().getX() + ", " + prey.getLocation().getY() + ", " + prey.getLocation().getZ() + "]");
+                                    //CompassTracker.getPlugin().getLogger().info(ChatColor.GREEN + "Now pointing to: " + ChatColor.GOLD + prey.getName() + ChatColor.RESET + " at [" + prey.getLocation().getX() + ", " + prey.getLocation().getY() + ", " + prey.getLocation().getZ() + "]");
                                 }
                             }
                         }.runTaskLater(CompassTracker.getPlugin(), delay);
                         Compass.toggle(); //Toggle runnable off
                     }
-                } else  CompassTracker.getPlugin().getLogger().severe(ChatColor.DARK_RED + "Could not find player!");
+                } else CompassTracker.getPlugin().getLogger().severe(ChatColor.DARK_RED + "Could not find player!");
             } else CompassTracker.getPlugin().getLogger().severe(ChatColor.RED + "Error finding your prey, please try /hunt again!");
         }
     }
